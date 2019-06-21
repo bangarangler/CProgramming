b7e91513d4433e7942c8e7c2931cc4926fca6e57
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-21 19:13:50 -0400

    printf()

--- FirstName/Sources/test.c
+++ FirstName/Sources/test.c
@@ -65,9 +65,32 @@ Date: 06/21/2019
   /*return 0;*/
 /*}*/
 
+/*int main()*/
+/*{*/
+  /*char myCharacter = '\n';*/
+  /*printf("%c", myCharacter);*/
+  /*return 0;*/
+/*}*/
+
+/*int main()*/
+/*{*/
+  /*int integerVar = 100;*/
+  /*float floatingVar = 331.79;*/
+  /*double doubleVar = 8.44e+11;*/
+  /*char charVar = 'W';*/
+  /*bool boolVar = false;*/
+
+  /*printf("integerVar = %i\n", integerVar);*/
+  /*printf("floatingVar = %f\n", floatingVar);*/
+  /*printf("floatingVar = %2.f\n", floatingVar);*/
+  /*printf("doubleVar = %e\n", doubleVar);*/
+  /*printf("doubleVar = %g\n", doubleVar);*/
+  /*printf("charVar = %c\n", charVar);*/
+  /*printf("boolVar = %i\n", boolVar);*/
+/*}*/
+
 int main()
 {
-  char myCharacter = '\n';
-  printf("%c", myCharacter);
-  return 0;
+  float x = 3.9993232323;
+  printf("%.2f", x);
 }
