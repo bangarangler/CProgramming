0b45bb75a8c28196b984e4946b55d1ce42f9cae5
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-21 12:41:25 -0400

    Scanf stuff and printf stuff

--- FirstName/Sources/test.c
+++ FirstName/Sources/test.c
@@ -1,7 +1,37 @@
+/*
+Author: Jon Palacio
+Purpose: This program prints out my name to screen
+Date: 06/21/2019
+*/
+
 #include <stdio.h>
+
+/*int main()*/
+/*{*/
+  /*// this code is displaying my name*/
+  /*printf("Hi, my name is Jon");*/
+  /*return 0;*/
+/*}*/
+
+/*int main()*/
+/*{*/
+  /*char str[100];*/
+  /*int i;*/
+
+  /*printf("Enter a value :");*/
+
+  /*scanf("%d %s", &i, str);*/
+
+  /*printf("\nYou entered: %d::::%s\n", i, str);*/
+
+
+  /*return 0;*/
+/*}*/
 
 int main()
 {
-  printf("Hi, my name is Jon");
+  double x;
+
+  scanf("%lf", &x);
   return 0;
 }
