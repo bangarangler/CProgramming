8afe23ec73f48392f5acdc0c7439d7a44799d94b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-21 20:08:09 -0400

    Slight tweak

--- Rectangle/Source/main.c
+++ Rectangle/Source/main.c
@@ -20,22 +20,29 @@
   */
 
 
+/*
+ * Author: Jon
+ * Purpose: This program is going to calculate the area of a rectangle
+   */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
   /*double width = 44.44;*/
   /*double height = 66.66;*/
-  double width = *argv[0];
-  double height = *argv[1];
+  double width = atoi(argv[1]);
+  double height = atoi(argv[2]);
   double perimeter = 0.0;
   double area = 0.0;
 
   perimeter = 2.0 * (height + width);
   area = width * height;
 
-  printf("The width is: %e\n", width);
-  printf("The height is: %e\n", height);
-  printf("The perimeter is: %g\n", perimeter);
-  printf("The area is: %g\n", area);
+  printf("Width is %.2f, Height is %.2f\n", width, height);
+  /*printf("The width is: %e\n", width);*/
+  /*printf("The height is: %e\n", height);*/
+  printf("The perimeter is: %.2f\n", perimeter);
+  printf("The area is: %.2f\n", area);
+  return 0;
 }
