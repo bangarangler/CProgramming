bb91b4beea7df816814a4c9a5f98c9a4ab7f2582
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-26 13:18:00 -0400

    Uppercase challenge files

---
+++ CDebugging/source/upperFile.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define FILENAME "randomText.txt"
+
+int main()
+{
+	FILE *fp = NULL;
+	FILE *fp1 = NULL;
+
+	char ch = ' ';
+
+	fp = fopen(FILENAME, "r");
+
+	if(fp == NULL)
+		return -1;
+
+	// create temp file
+	fp1 = fopen("temp.txt", "w");
+
+	if (fp1 == NULL)
+		return -1;
+
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		if (islower(ch))
+		{
+			ch = ch-32;
+		}
+
+		fputc(ch, fp1);
+	}
+
+	fclose(fp);
+	fclose(fp1);
+
+	// rename temp file to randomText.txt file
+
+	rename("temp.txt", FILENAME);
+
+	// remove temp file
+	remove("temp.txt");
+	fp = fopen(FILENAME, "r");
+
+	if (fp == NULL)
+		return -1;
+
+	while ((ch = fgetc(fp)) != EOF)
+		printf("%c", ch);
+
+	fclose(fp);
+	fp = NULL;
+	fp1 = NULL;
+
+	return 0;
+}
