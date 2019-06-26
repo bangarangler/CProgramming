1a986b391f06358a322108c8ab1430cdee6086b5
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-26 09:00:03 -0400

    Struct Challenge

---
+++ CDebugging/source/structChallenge.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct employee {
+	char name[30];
+	char date[15];
+	float salary;
+};
+
+int main()
+{
+	struct employee emp={"Mike", "7/16/2015", 76909.00f};
+
+	printf("\n  Name:  %s"   ,emp.name);
+	printf("\n  Hire Date:   %s"   , emp.date);
+	printf("\n  Salary:   %.2f\n",emp.salary);
+
+	printf("\nEnter employee information: \n");
+
+	printf("Name: ");
+	scanf("%s", emp.name);
+
+	printf("\nHire Date: ");
+	scanf("%s", emp.date);
+
+	printf("\n Salary: ");
+	scanf("%f", &emp.salary);
+
+	printf("\n  Name:  %s"   ,emp.name);
+	printf("\n  Hire Date:   %s"   , emp.date);
+	printf("\n  Salary:   %.2f\n",emp.salary);
+	return 0;
+}
